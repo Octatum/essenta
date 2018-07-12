af54bd6ed9b79f895a313bee72cfa459bf5ec6ef
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-07-12 12:28:46 -0500

    Removed cache folder from tracking

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "C:/Users/robil/Sofware/Octatum/essenta/essenta-gatsby/src/layouts/index.jsx"
-  import data from "C:\\Users\\robil\\Sofware\\Octatum\\essenta\\essenta-gatsby\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
