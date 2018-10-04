dcdaee7d4cd29d9f1f11216439ed6f0d833a6211
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-10-04 01:44:05 -0500

    cool

---
+++ src/components/Footer/NewsForm.jsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { TextInput } from '../Input';
+import Button from '../Button';
+import { device } from '../../utilities/device';
+
+const Layout = styled.div`
+  display: flex;
+
+  ${device.laptop} {
+    flex-direction: column;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex: 3;
+  justify-content: space-between;
+
+  ${device.mobile} {
+    flex-direction: column;
+  }
+`;
+
+const ContactInfo = styled.div`
+  display: flex;
+  flex: 2;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.2rem;
+  box-sizing: border-box;
+  padding: 0 2rem;
+
+  ${device.laptop} {
+    margin-top: 1em;
+  }
+
+  ${device.mobile} {
+    padding: 0;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+
+const CallUs = styled.div`
+  font-family: ${props => props.theme.fonts.secondary};
+  color: ${({ theme }) => theme.color.black};
+
+  ${device.mobile} {
+    min-width: 100%;
+  }
+`;
+
+const SocialButton = styled.a`
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${props => props.theme.color.gray};
+  background: ${props => props.theme.color.orange};
+  border-radius: 40%;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  width: 2.3rem;
+  transition: 300ms ease background-color;
+
+  &:visited {
+    text-decoration: none;
+  }
+
+  &:focus {
+    background: ${props => props.theme.color.darkOrange};
+  }
+`;
+
+const Bold = styled.span`
+  font-weight: 700;
+`;
+
+const CustomButton = Button.extend`
+  flex: 4;
+  margin-left: 1em;
+  font-size: 1.3rem;
+  text-align: center;
+
+  ${device.mobile} {
+    margin: 0;
+  }
+`;
+
+const CustomTextInput = TextInput.extend`
+  flex: 6;
+  font-size: 1rem;
+  text-align: center;
+
+  ${device.mobile} {
+    padding: 0.5em 0;
+    margin-bottom: 1em;
+  }
+`;
+
+const SocialButtons = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-around;
+
+  ${device.tablet} {
+    padding-top: 0.5em;
+    width: 100%;
+  }
+`;
+
+class NewsForm extends React.Component {
+  state = {
+    email: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  submitForm = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Form action="" name="contacto" netlify>
+          <CustomTextInput
+            type="email"
+            placeholder="Recibe las últimas noticias de ESSENTA"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <CustomButton onClick={this.submitForm}>Suscríbete</CustomButton>
+        </Form>
+        <ContactInfo>
+          <CallUs>
+            <Bold>LLÁMANOS:</Bold>
+            &nbsp;&nbsp;55-1000-2866
+          </CallUs>
+          <SocialButtons>
+            <SocialButton target="_blank" href="https://facebook.com/essentamx">
+              <i className="fab fa-facebook-f" />
+            </SocialButton>
+            {/* <SocialButton target="_blank" href="/"><i className="fab fa-twitter"/></SocialButton>
+            <SocialButton target="_blank" href="/"><i className="fab fa-youtube"/></SocialButton> */}
+          </SocialButtons>
+        </ContactInfo>
+      </Layout>
+    );
+  }
+}
+
+export default NewsForm;
---
+++ src/pages/producto.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import ProductPickerContainer from '../components/Product';
+import AppLayout from '../components/AppLayout';
+
+function getCleanFragancesData(fragancesResult) {
+  return fragancesResult.edges.map(({ node }) => ({ ...node }));
+}
+
+function groupFragancesByFamily(fragances) {
+  const families = {};
+
+  fragances.forEach(fragance => {
+    if (!!!families[fragance.family]) {
+      families[fragance.family] = [];
+    }
+
+    families[fragance.family].push(fragance);
+  });
+
+  return families;
+}
+
+// Gets product and fragance data and passes it into the corresponding view
+function ProductPickerTemplate({ data }) {
+  const { containers, fragancesResults } = data;
+  const fragances = getCleanFragancesData(fragancesResults);
+  const groupedFragances = groupFragancesByFamily(fragances);
+  const cleanContainersData = containers.edges.map(({ node }) => ({ ...node }));
+
+  return (
+    <AppLayout>
+      <ProductPickerContainer
+        data={{
+          fragances: groupedFragances,
+          containers: cleanContainersData,
+        }}
+      />
+    </AppLayout>
+  );
+}
+
+export default ProductPickerTemplate;
+
+export const pageQuery = graphql`
+  query AllProductsAndFragances {
+    containers: allContentfulRecipiente {
+      edges {
+        node {
+          id
+          label
+          gender
+          price: sizePrice
+          category {
+            path
+            title
+          }
+          colores {
+            id
+            colorName
+            image {
+              fluid {
+                ...GatsbyContentfulFluid
+              }
+            }
+          }
+        }
+      }
+    }
+
+    fragancesResults: allContentfulFragancia {
+      edges {
+        node {
+          id
+          displayName
+          objectiveGender
+          family
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+          suggestions {
+            suggestions
+          }
+          description {
+            description
+          }
+        }
+      }
+    }
+  }
+`;
