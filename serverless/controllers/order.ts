6efc827c03305897e2d8db0ea39b349ccde650b1
RobRuizR <rroberto.rruiz@gmail.com>
2018-12-10 22:49:00 -0600

    Functions

---
+++ serverless/controllers/order.ts
@@ -0,0 +1,115 @@
+import { Request, Response } from "express";
+import { validationResult } from "express-validator/check";
+import ip from "ip";
+import FormData from "form-data";
+import fetch, { Headers } from "node-fetch";
+import { Buffer } from "buffer";
+
+import * as OrderModel from "../models/Order";
+import * as ProductModel from "../models/Product";
+import logger from "../util/logger";
+
+export async function allOrders(req: Request, res: Response): Promise<any> {
+  logger.log("info", "Listing all orders");
+  const orders = await OrderModel.getAllOrders();
+
+  res.json(orders);
+}
+
+export async function createOrder(req: Request, res: Response): Promise<any> {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  logger.log("info", "Creating order");
+
+  const { items, customer } = req.body;
+
+  try {
+    const orderId: String = await OrderModel.createOrder(items, customer);
+    const orderItems: any[] = await Promise.all(
+      items.map((item: OrderModel.APIItem) => ProductModel.getProduct(item))
+    );
+    const totalCost: Number = orderItems.reduce(
+      (accum, current) => current.price * current.amount + accum,
+      0
+    );
+    const redirectionUrl: String = await getRedirectionUrl(orderId, totalCost);
+    res.json({ orderId, redirectionUrl });
+  } catch (error) {
+    logger.log("error", error);
+    res.sendStatus(400);
+  }
+}
+
+const testItems = [{
+  "id": "5YPO1Smo3mcMoqkMcGOK0W",
+  "amount": "23",
+  "colorId": "4Cjuek1kdqgGswW0iOwawc",
+  "fraganceId": "52fomcNjNKAimUeOuM2QOq"
+},
+{
+  "id": "69sC7kthhSEkYscyQu8WEQ",
+  "amount": "2",
+  "colorId": "592DUMfwiQQ64egUsOOaI4",
+  "fraganceId": "52fomcNjNKAimUeOuM2QOq"
+}
+];
+
+export async function testCreateOrder(req: Request, res: Response) {
+  try {
+    const orderId: String = await OrderModel.createOrder(testItems, {});
+    const orderItems: any[] = await Promise.all(
+      testItems.map((item: any) => ProductModel.getProduct(item))
+    );
+    const totalCost: Number = orderItems.reduce(
+      (accum, current) => current.price * current.amount + accum,
+      0
+    );
+    const redirectionUrl: String = await getRedirectionUrl(orderId, totalCost);
+    res.json({ orderId, redirectionUrl });
+  } catch (error) {
+    logger.log("error", error);
+    res.sendStatus(400);
+  }
+}
+
+async function getRedirectionUrl(orderId: String, totalCost: Number) {
+  const formData = new FormData();
+  const headers = new Headers();
+  const username = process.env.TP_USER;
+  const password = process.env.TP_PASS;
+  const hostUrl = process.env.TP_URL;
+
+  // Preparar datos y hacer encode como Form (necesario para TP)
+  formData.append("CONTROL_NUMBER", orderId.toString());
+  formData.append("AMOUNT", totalCost.toString());
+  formData.append("ADDRESS", ip.address());
+
+  const base64AuthData: string = Buffer.from(
+    `${username}:${password}`
+  ).toString("base64");
+
+  headers.append("AUTHORIZATION", `Basic ${base64AuthData}`);
+
+  const response = await fetch(hostUrl, {
+    method: "POST",
+    body: formData,
+    headers,
+  });
+
+  const jsonResponse = await response.json();
+
+  let redirectionUrl: String = jsonResponse.msgError;
+
+  redirectionUrl = redirectionUrl.slice(17, -2);
+
+  return redirectionUrl;
+}
+
+export async function updateOrder(req: Request, res: Response) {
+  logger.log("info", "Updating order:", req);
+  res.sendStatus(200);
+}
---
+++ serverless/models/Order.ts
@@ -0,0 +1,60 @@
+import firebaseDb from "../util/firebaseDb";
+
+const COLLECTION_NAME = "order";
+
+export type APIItem = {
+  id: string;
+  amount: number;
+  colorId: string;
+  fraganceId: string;
+};
+
+export type Order = {
+  id: string;
+  items: APIItem[];
+  customer: any;
+};
+
+export type OrderModel = {
+  getAllOrders: getAllOrdersFunction;
+  getOrderById: getOrderByIdFunction;
+  createOrder: createOrderFunction;
+};
+
+type getAllOrdersFunction = () => Promise<Order[]>;
+type getOrderByIdFunction = (orderId: string) => Order;
+type createOrderFunction = (items: APIItem[], customer: any) => String;
+
+const orderCollectionRef = firebaseDb.collection(COLLECTION_NAME);
+
+export async function createOrder(items: any[], customer: any) {
+  // TODO: usar customer
+  const orderRef = await orderCollectionRef.add({
+    items,
+    status: OrderStatus.waiting,
+  });
+
+  return orderRef.id;
+}
+
+export enum OrderStatus {
+  canceled = -1,
+  waiting = 0,
+  approved = 1,
+}
+
+export async function getAllOrders() {
+  const snapshot = await orderCollectionRef.get();
+
+  const allOrders: Order[] = [];
+
+  snapshot.forEach((order: FirebaseFirestore.QueryDocumentSnapshot) => {
+    allOrders.push({
+      id: order.id,
+      items: order.data().items,
+      customer: order.data().customer,
+    });
+  });
+
+  return allOrders;
+}
