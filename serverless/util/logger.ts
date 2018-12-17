526fdfdb2f04eb34b61db2f72964bc03bacf5fd8
RobRuizR <rroberto.rruiz@gmail.com>
2018-12-17 11:32:38 -0600

    Removed logger file

--- serverless/util/logger.ts
+++ serverless/util/logger.ts
@@ -5,8 +5,7 @@ const logger = new Logger({
   transports: [
     new winston.transports.Console({
       level: process.env.NODE_ENV === "production" ? "error" : "debug",
-    }),
-    new winston.transports.File({ filename: "debug.log", level: "debug" }),
+    })
   ],
 });
 
